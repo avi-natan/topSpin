17ca43bd91d1d57f583d61156187beaa6edfb0c6
Avraham Natan <avinat123@gmail.com>
2023-06-08 20:20:43 +0300

    part 1 (defining state) complete

--- topspin.py
+++ topspin.py
@@ -2,13 +2,19 @@
 class TopSpinState:
 
     def __init__(self, state, k=4):
-        pass
+        self.k = k
+        self.state = state
 
     def is_goal(self):
-        pass
+        return sorted(self.state) == self.state
 
     def get_state_as_list(self):
-        pass
+        return self.state
 
     def get_neighbors(self):
-        pass
+        # 3 neighbours: left rotate, right rotate, and k flip
+        l_rotate = self.state[1:] + [self.state[0]]
+        r_rotate = [self.state[-1]] + self.state[:-1]
+        k_flip = self.state[:self.k][::-1] + self.state[self.k:]
+
+        return [TopSpinState(l_rotate,self.k), TopSpinState(r_rotate,self.k), TopSpinState(k_flip,self.k)]
